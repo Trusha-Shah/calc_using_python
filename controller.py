cd427884531bb9be8c17c91986cdd15f5c0130c9
Trusha-Shah <61858937+Trusha-Shah@users.noreply.github.com>
2020-09-29 11:02:58 +0530

    Created controller.py and Updated main.py

---
+++ controller.py
@@ -0,0 +1,30 @@
+from functools import partial
+ERROR_MSG = 'ERROR'
+
+class Controller:
+	'''Controller for Calculator'''
+	def __init__(self, model, view):
+		'''Controller initializer'''
+		self._evaluate = model
+		self._view = view
+		self._connectSignals()
+
+	def _buildExpression(self, sub_exp):
+		'''Build Expression'''
+		if self._view == ERROR_MSG:
+			self._view.clearDisplay()
+
+		expression = self._view.getDisplayText() + sub_exp
+		self._view.setDisplayText(expression)
+
+	def _connectSignals(self):
+		for btnText,btn in self._view.buttons.items():
+			if btnText not in {'=','C'}:
+				btn.clicked.connect(partial(self._buildExpression, btnText))
+		self._view.buttons['='].clicked.connect(self._calculateResult)
+		self._view.display.returnPressed.connect(self._calculateResult)
+		self._view.buttons['C'].clicked.connect(self._view.clearDisplay)
+	def _calculateResult(self):
+		'''Evaluate Expression'''
+		result = self._evaluate(expression = self._view.getDisplayText())
+		self._view.setDisplayText(result)
